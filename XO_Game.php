781140dd64e48479f2f060c0f087f5ed8f27a47a
seeker24 <golsarchat@chmail.ir>
2016-09-08 11:08:18 +0430

    Update XO_Game.php

--- XO_Game.php
+++ XO_Game.php
@@ -1,6 +1,6 @@
 ﻿<?php
 
-define('BOT_TOKEN', 'Your Token');
+define('BOT_TOKEN', '260035372:AAGpahKfi9_vLE6kjFjSTOi9t9h0vVbRjdE');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
@@ -151,8 +151,8 @@ function processMessage($message) {
     if (strpos($text, "/start") === 0) {
       apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => "اگه تو هم تلگرامت آپدیت باشه این دکمه های شیشه ای رو دیدی صددرصد\n ما با همین دکمه ها براتون چند تا بازی آماده کردیم مثل ایکس او و سنگ کاغذ قیچی \n اگه مایلی با دوستات بازی کنی روی لینک زیر کلیک کن. بعد دوستتو انتخاب کن و منتظر باش که بااکس اینلاین ظاهر شه", 'reply_markup' => array(
             "inline_keyboard"=>array(
-			    array(array("text"=>"توسعه دهنده","url"=>"https://telegram.me/msmsepehr")),
-				array(array("text"=>"مشارکت کننده","url"=>"https://telegram.me/RaminaO_O")),
+			    array(array("text"=>"کانال ما","url"=>"https://telegram.me/owners_groups")),
+				array(array("text"=>"سفارش ربات ضداسپم وپیامرسان","url"=>"https://telegram.me/golsarpm_bot")),
 			    array(array("text"=>"بازی XO","switch_inline_query"=>md5(date("YMDms"))),array("text"=>"سنگ کاغذ قیچی (به زودی)","callback_data"=>"m"))
 			)
 		)));
